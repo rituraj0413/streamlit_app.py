b0439a51e24e79739d0554e20d3d2ff252bdf116
RITU RAJ <115033344+rituraj0413@users.noreply.github.com>
2023-11-06 10:20:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+
+# Sample product data (you would typically use a database)
+products = [
+    {"name": "T-shirt", "price": 20.0, "image": "tshirt.jpg"},
+    {"name": "Dress", "price": 40.0, "image": "dress.jpg"},
+    # Add more products here
+]
+
+st.title("Clothing Design Store")
+
+for product in products:
+    st.write(f"**{product['name']}**")
+    st.image(product['image'])
+    st.write(f"Price: ${product['price']}")
+
+# Add more Streamlit components as needed
 
